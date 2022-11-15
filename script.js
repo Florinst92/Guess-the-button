3e33d173cda1650612b2b2b8536c15475b9f9e7e
Florinst92 <70140191+Florinst92@users.noreply.github.com>
2022-11-15 19:52:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-# Guess-the-button
-Guess the button
+  <script>
+  function guess_the_Button(input) {
+    const x = document.getElementById('showmessage');
+    x.innerHTML="";
+    const winner = document.createElement('div');
+    winner.textContent = 'You guessed the winner button!';
+    const loser = document.createElement('div');
+    loser.textContent = 'You pressed the looser button!';
+    let random = Math.floor(Math.random() * 3);
+    if (input == random) {
+      x.appendChild(winner);
+    } else {
+      x.appendChild(loser)
+    }
+  }
+</script>
